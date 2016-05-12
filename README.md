efacefdc00974306e931eb33948510f843b0305b
W. Perry Wortman <pwortman@visioncreativegroup>
2016-05-12 15:24:47 -0400

    first commit

--- README.md
+++ README.md
@@ -1,46 +1,4 @@
-#Express Basics Demo Site
-
-This repo features an express app to serve as demo in the Express Basics course on Treehouse.
-
-## How to use this repo
-To follow along with the Treehouse course, checkout the branch with the associated video.  The video for a given stage will indicate which branch to checkout.
-
-**To start with the very first video:**
-```
-git clone <this-repo>
-cd <this-repo>
-# you should already be on the master branch, but just in case
-git checkout master
-```
-
-**To begin with progress up until a certain video, for example "S2V2":**
-```
-# from this repo
-git checkout S2V2
-```
-These instructions are outlined in individual videos. 
-
-**To view the final app:**
-```
-# from this repo
-git checkout final
-```
-
-## Set Up
-```
-# clone the repo
-git clone <this-repo>
-cd <this-repo>
-
-# install node dependencies
-npm install
-
-# view the app
-# Note: to develop, see instructions below
-node src
-```
-
-## Developing
+# Setup
 ```
 # make sure you have some popular node tools installed
 npm install -g nodemon
--- src/app.js
+++ src/app.js
@@ -1,28 +1,38 @@
 'use strict';
 
 var express = require('express'),
-	  posts = require('./mock/posts.json');
+		port = 3002,
+	  posts = require('./mock/posts.json'),
+		postsList = Object.keys(posts).map(function(value){
+	return posts[value];
+});
 
 var app = express();
+
+app.use('/static', express.static(__dirname + '/public'));
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/templates');
 
 app.get('/', function(req, res){
+	// req.path ex: a req to '/blog' or '/'
+	var path = req.path;
+	// res.locals stores variables that will be accessible in the template
+	res.locals.path = path;
 	res.render('index');
 });
 
-app.get('/blog/:title?', function(req, res){ 
+app.get('/blog/:title?', function(req, res){
 	var title = req.params.title;
 	if (title === undefined) {
 		res.status(503);
-		res.send("This page is under construction!");
+		res.render('blog', {posts: postsList});
 	} else {
 		var post = posts[title] || {};
-		res.render('post', { post: post});
+		res.render('post', {post: post});
 	}
 });
 
-app.listen(3000, function() {
-	console.log("The frontend server is running on port 3000!");
+app.listen(port, function() {
+	console.log("The frontend server is running on " + port + "!");
 });
